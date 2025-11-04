c0b78ef6b24adf246b4b59a1716a4539211cd987
Juan Cárdenas Zabala <juan.cardenas@endava.com>
2025-11-04 11:30:43 -0500

    feat: 🎸 added Logo component (text = white/black)
    This component provides the option of using custom className for size
    and change easily between black or white logo letters and visual
    elements (useful for dark/clear themes)

---
+++ src/components/atoms/Logo.tsx
@@ -0,0 +1,11 @@
+const Logo = ({ className = "w-24 h-auto" , text = "white"}) => {
+  return (
+    <img
+      src={`/logo-${text}-text.png`}
+      alt="Logo"
+      className={className}
+    />
+  );
+};
+
+export default Logo;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,10 @@
+import Logo from "../components/atoms/Logo";
+
 export const HomePage = () => {
     return (
         <div className="bg-gray-800">
             <h1 className="text-white">Welcome to the Home Page</h1>
+            <Logo text="white" />
         </div>
     );
 }
