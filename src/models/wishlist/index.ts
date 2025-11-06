1bf02ae0ec0c3b6b2dc14cce0beedbde2f0949f9
Juan Sebastián Dosman <sebas_dosman@Juans-MacBook.local>
2025-11-06 14:35:59 -0500

    feat: 🎸 all back-end services
    Add all back-end services

---
+++ src/models/wishlist/index.ts
@@ -0,0 +1,5 @@
+export * from "./add-wishlist-item.request.interface";
+export * from "./wishlist-item-raw.response.interface";
+export * from "./wishlist-item.response.type";
+export * from "./wishlist-raw.response.interface";
+export * from "./wishlist.response.type";
